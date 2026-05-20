e0c43a2d78300ac31126ec95056e80736d0edc0e
Arthur Ribeiro <ribax.dev@gmail.com>
2026-05-20 16:28:08 -0300

    feat: improve code and move new socket for enter the for loop

--- src/main/java/org/project/Main.java
+++ src/main/java/org/project/Main.java
@@ -1,30 +1,35 @@
 package org.project;
 
 
+import java.io.IOException;
+import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.Socket;
 
 public class Main {
-    public static void main(String[] args){
-        final String host ="2775ARTHURNASCIMENTO";
-        final int totalPorts = 10;
-        final int timeoutMs = 200;
+    public static void main(String[] args) throws IOException {
 
-        System.out.println("Start scanning on: " + host)a;
+        final String host = InetAddress.getLocalHost().getHostAddress();
+        final int totalPorts = 65535;
+
+        System.out.println("Start scanning on: " + host);
+
         for (int port = 0; port <= totalPorts; port++) {
             printProgress(port, totalPorts);
+
             try {
-                Socket socket = new Socket(host, port);
-                socket.connect(new InetSocketAddress(host, port), timeoutMs);
+                Socket socket = new Socket();
+                socket.connect(new InetSocketAddress(host, port), 200);
 
                 System.out.print("\r\033[K");
                 System.out.println("[+] Port " + port + " is OPEN");
                 socket.close();
+                socket = null;
             } catch (Exception ignored){}
         }
     }
     private static void printProgress(int current, int totalPorts) {
-        int progressPercent = (int) (((double) current / 10) * 100);
+        int progressPercent = (int) (((double) current / totalPorts) * 100);
         int barLength = 30;
         int completedBars = (int) (((double) current / totalPorts) * barLength);
 
@@ -38,6 +43,6 @@ public class Main {
         }
         bar.append("]");
 
-        System.out.print("\r" + bar + " " + progressPercent + "% (Porta: " + current + "/" + 65535 + ")");
+        System.out.print("\r" + bar + " " + progressPercent + "% (Porta: " + current + "/" + totalPorts+ ")");
     }
 }
